1cfdcfb929a4c0ec652aaad35b8a3a3df5225609
chenjie <4234115@qq.com>
2019-04-19 11:26:21 +0800

    StarRate

---
+++ StarRateView/StarType.swift
@@ -0,0 +1,19 @@
+//
+//  StarType.swift
+//  StarRateView
+//
+//  Created by mac on 2019/4/1.
+//  Copyright © 2019 Jason. All rights reserved.
+//
+
+import Foundation
+
+public enum StarType{
+
+    /// 整颗星星
+    case `default`
+    /// 半颗星星
+    case half
+    /// 不限制
+    case unlimited
+}
